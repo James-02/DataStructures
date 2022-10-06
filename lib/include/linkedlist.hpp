4544a797f61b7939d7c8a4eef0f4060bf054f0a5
James Newsome <james.newsome02@gmail.com>
2022-10-06 23:21:50 +0100

    Complete LinkedList implementation with exception throwing and header docstrings

--- lib/include/linkedlist.hpp
+++ lib/include/linkedlist.hpp
@@ -4,26 +4,75 @@
 struct Node {
     int data;
     Node* next;
+    Node* prev;
+    Node(int data) : data(data) {}
 };
 
 class LinkedList {
-
-    public:
+    private: 
+        int size;
         Node* head;
         Node* tail;
-        
+
+    public:
         LinkedList() {
+            size = 0;
             head = nullptr;
             tail = nullptr;
         }
+        /**
+         * @brief Append a node onto the end of the Linked List, this node becomes the tail.
+         * @param data data to initialise node with
+         * 
+         */
+        void append(int data);
 
-        void append(Node n);
-        void push(Node n);
-        void insert(Node n, int pos);
-        void remove(Node n, int pos);
-        Node search(Node n);
-        void update(Node n);
-        void traverse();
+        /**
+         * @brief Pushes a node to the start of the Linked List, this node becomes the head.
+         * @param data data to initialise the node with.
+         * 
+         */
+        void push(int data);
+
+        /**
+         * @brief Inserts a node at the position given.
+         * @param pos position to insert node at.
+         * @param data data to initialise node with.
+         * @throws std::out_of_bounds()
+         * 
+         */
+        void insert_at(int pos, int data);
+
+        /**
+         * @brief Removes the node at the position given.
+         * @param pos position to remove Node from.
+         * 
+         */
+        void remove(int pos);
+
+        /**
+         * @brief Returns the node at the given position.
+         * @param pos position to search for a node.
+         * @return Pointer to the node at that position.
+         * @throws std::out_of_bounds()
+         * 
+         */
+        Node* search(int pos);
+
+        /**
+         * @brief Updates the data of the Node at the given position using the data parameter.
+         * @param pos position to update node at.
+         * @param data data to update node to hold.
+         * 
+         */
+        void update(int pos, int data);
+
+        /**
+         * @brief Prints each element of the Linked List with a pointer to the next element.
+         * 
+         * 
+         */      
+        void print();
 
 };
 
--- lib/src/linkedlist.cpp
+++ lib/src/linkedlist.cpp
@@ -1,33 +1,117 @@
 #include <iostream>
 #include "linkedlist.hpp"
 
-
-void LinkedList::append(Node n) {
-
+void LinkedList::push(int data) {
+    Node *new_node = new Node(data);
+    // check if head exists
+    if (this->head) {
+        // link head to new node
+        new_node->next = this->head;
+        this->head->prev = new_node;
+    }
+    else {
+        // set tail to new_node which will also be the head
+        this->tail = new_node;
+    }
+    // set new_node to head
+    this->head = new_node;
+    size++;
 }
 
-void LinkedList::push(Node n) {
+void LinkedList::append(int data) {
+    Node *new_node = new Node(data);
 
+    this->tail->next = new_node;
+    new_node->prev = this->tail;
+
+    this->tail = new_node;
+    new_node->next = nullptr;
+    size++;
 }
 
-void LinkedList::insert(Node n, int pos) {
+void LinkedList::insert_at(int pos, int data) {
+    if (pos > size || pos < 0) {
+        throw std::out_of_range("Position out of current Linked List's range.");
+    }
 
+    Node *prev_node;
+    switch(pos) {
+        case 0:
+            LinkedList::push(data);
+            return;
+        case 1:
+            prev_node = this->head;
+            break;
+        default:
+            prev_node = LinkedList::search(pos-1);
+            break;
+    }
+
+    Node *new_node = new Node(data);
+
+    if (this->tail == prev_node) {
+        prev_node->next = new_node;
+        new_node->prev = prev_node;
+        this->tail = new_node;
+        new_node->next = nullptr;
+    }
+    else {
+        // set new_node's prev & next
+        new_node->next = prev_node->next;
+        new_node->prev = prev_node;
+
+        // fix surrounding node's pointers
+        new_node->next->prev = new_node;
+        prev_node->next = new_node;
+    }
+    size++;
 }
 
-void LinkedList::remove(Node n, int pos) {
+void LinkedList::remove(int pos) {
+    Node *node = LinkedList::search(pos);
+    if (this->head == node) {
+        this->head = this->head->next;
 
+    }
+    else if (this->tail == node) {
+        node->prev->next = nullptr;
+        this->tail = node->prev;
+    }
+
+    else {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+    delete node;
+    size--;
 }
 
-Node LinkedList::search(Node n) {
-    return n;
+Node* LinkedList::search(int pos) {
+    if (pos >= size || pos < 0) {
+        throw std::out_of_range("Position out of current Linked List's range.");
+    }
 
+    Node *current = this->head;
+    int i = 0;
+    // get each node's next node until we reach the desired position or tail.
+    while (current != nullptr && i != pos) {
+        current = current->next;
+        i++;
+    }
+
+    return current;
 }
 
-void LinkedList::update(Node n) { 
-
+void LinkedList::update(int pos, int data) { 
+    Node *node = LinkedList::search(pos);
+    node->data = data;
 }
 
-void LinkedList::traverse() {
-
+void LinkedList::print() {
+    Node *node = this->head;
+    while (node != nullptr) {
+        std::cout << node->data << "-->";
+        node = node->next;
+    }
+    std::cout << "NULL" << std::endl;
 }
-
