197cb2be4ad0caa77dbf389a6d3702f8d453f10f
James Newsome <james.newsome02@gmail.com>
2022-10-10 23:49:37 +0100

    Added template implementation for generic types

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include "linkedlist.hpp"
 
 int main() {
-    LinkedList l;
+    LinkedList<int> l;
     l.push(23);
     l.push(14);
     l.append(19);
-    l.insert_at(3, 54);
+    l.insert_at(1, 54);
 
-    Node *n = l.search(2);
+    Node<int>*n = l.search(1);
+    std::cout << "Data: " << n->data << std::endl;
 
     l.print();
     l.remove(0);
--- src/linkedlist.cpp
+++ src/linkedlist.cpp
@@ -1,117 +1,120 @@
-#include <iostream>
-#include "linkedlist.hpp"
+// #include <iostream>
+// #include "linkedlist.hpp"
 
-void LinkedList::push(int data) {
-    Node *new_node = new Node(data);
-    // check if head exists
-    if (this->head) {
-        // link head to new node
-        new_node->next = this->head;
-        this->head->prev = new_node;
-    }
-    else {
-        // set tail to new_node which will also be the head
-        this->tail = new_node;
-    }
-    // set new_node to head
-    this->head = new_node;
-    size++;
-}
+// template <typename T>
+// void LinkedList<T>::push(const T& data) {
+//     Node<T> *new_node = new Node<T>(data);
+//     // check if head exists
+//     if (this->head) {
+//         // link head to new node
+//         new_node->next = this->head;
+//         this->head->prev = new_node;
+//     }
+//     else {
+//         // set tail to new_node which will also be the head
+//         this->tail = new_node;
+//     }
+//     // set new_node to head
+//     this->head = new_node;
+//     size++;
+// }
+// template <typename T>
+// void LinkedList<T>::append(const T& data) {
+//     Node<T> *new_node = new Node<T>(data);
 
-void LinkedList::append(int data) {
-    Node *new_node = new Node(data);
+//     this->tail->next = new_node;
+//     new_node->prev = this->tail;
 
-    this->tail->next = new_node;
-    new_node->prev = this->tail;
+//     this->tail = new_node;
+//     new_node->next = nullptr;
+//     size++;
+// }
+// template <typename T>
+// void LinkedList<T>::insert_at(int pos, const T& data) {
+//     if (pos > size || pos < 0) {
+//         throw std::out_of_range("Position out of current Linked List's range.");
+//     }
 
-    this->tail = new_node;
-    new_node->next = nullptr;
-    size++;
-}
+//     Node<T> *prev_node;
+//     switch(pos) {
+//         case 0:
+//             LinkedList::push(data);
+//             return;
+//         case 1:
+//             prev_node = this->head;
+//             break;
+//         default:
+//             prev_node = LinkedList::search(pos-1);
+//             break;
+//     }
 
-void LinkedList::insert_at(int pos, int data) {
-    if (pos > size || pos < 0) {
-        throw std::out_of_range("Position out of current Linked List's range.");
-    }
+//     Node<T> *new_node = new Node<T>(data);
 
-    Node *prev_node;
-    switch(pos) {
-        case 0:
-            LinkedList::push(data);
-            return;
-        case 1:
-            prev_node = this->head;
-            break;
-        default:
-            prev_node = LinkedList::search(pos-1);
-            break;
-    }
+//     if (this->tail == prev_node) {
+//         prev_node->next = new_node;
+//         new_node->prev = prev_node;
+//         this->tail = new_node;
+//         new_node->next = nullptr;
+//     }
+//     else {
+//         // set new_node's prev & next
+//         new_node->next = prev_node->next;
+//         new_node->prev = prev_node;
 
-    Node *new_node = new Node(data);
+//         // fix surrounding node's pointers
+//         new_node->next->prev = new_node;
+//         prev_node->next = new_node;
+//     }
+//     size++;
+// }
+// template <typename T>
+// void LinkedList<T>::remove(int pos) {
+//     Node<T> *node = LinkedList::search(pos);
+//     if (this->head == node) {
+//         this->head = this->head->next;
 
-    if (this->tail == prev_node) {
-        prev_node->next = new_node;
-        new_node->prev = prev_node;
-        this->tail = new_node;
-        new_node->next = nullptr;
-    }
-    else {
-        // set new_node's prev & next
-        new_node->next = prev_node->next;
-        new_node->prev = prev_node;
+//     }
+//     else if (this->tail == node) {
+//         node->prev->next = nullptr;
+//         this->tail = node->prev;
+//     }
 
-        // fix surrounding node's pointers
-        new_node->next->prev = new_node;
-        prev_node->next = new_node;
-    }
-    size++;
-}
+//     else {
+//         node->prev->next = node->next;
+//         node->next->prev = node->prev;
+//     }
+//     delete node;
+//     size--;
+// }
 
-void LinkedList::remove(int pos) {
-    Node *node = LinkedList::search(pos);
-    if (this->head == node) {
-        this->head = this->head->next;
+// template <typename T>
+// Node<T>* LinkedList<T>::search(int pos) {
+//     if (pos >= size || pos < 0) {
+//         throw std::out_of_range("Position out of current Linked List's range.");
+//     }
 
-    }
-    else if (this->tail == node) {
-        node->prev->next = nullptr;
-        this->tail = node->prev;
-    }
+//     Node<T> *current = this->head;
+//     int i = 0;
+//     // get each node's next node until we reach the desired position or tail.
+//     while (current != nullptr && i != pos) {
+//         current = current->next;
+//         i++;
+//     }
 
-    else {
-        node->prev->next = node->next;
-        node->next->prev = node->prev;
-    }
-    delete node;
-    size--;
-}
+//     return current;
+// }
 
-Node* LinkedList::search(int pos) {
-    if (pos >= size || pos < 0) {
-        throw std::out_of_range("Position out of current Linked List's range.");
-    }
-
-    Node *current = this->head;
-    int i = 0;
-    // get each node's next node until we reach the desired position or tail.
-    while (current != nullptr && i != pos) {
-        current = current->next;
-        i++;
-    }
-
-    return current;
-}
-
-void LinkedList::update(int pos, int data) { 
-    Node *node = LinkedList::search(pos);
-    node->data = data;
-}
-
-void LinkedList::print() {
-    Node *node = this->head;
-    while (node != nullptr) {
-        std::cout << node->data << "-->";
-        node = node->next;
-    }
-    std::cout << "NULL" << std::endl;
-}
+// template <typename T>
+// void LinkedList<T>::update(int pos, const T& data) { 
+//     Node<T> *node = LinkedList::search(pos);
+//     node->data = data;
+// }
+// template <typename T>
+// void LinkedList<T>::print() {
+//     Node<T> *node = this->head;
+//     while (node != nullptr) {
+//         std::cout << node->data << "-->";
+//         node = node->next;
+//     }
+//     std::cout << "NULL" << std::endl;
+// }
